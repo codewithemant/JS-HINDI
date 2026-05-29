8647144a9e21d93cb4e38c763184fd56ecba7fc5
codewithemant <codewithemant@gmail.com>
2026-05-29 18:24:34 +0530

    conversion of dataypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = true
+// const {score} = req.body
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN
+// true => NAN  
+// undefined => NAN 
+
+
+// let isLoggedIn = "true"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hemant" => true
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
