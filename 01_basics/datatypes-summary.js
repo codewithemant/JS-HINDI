b20c4555924a1b80c3bad2a9ed984ae0bb678fd3
codewithemant <codewithemant@gmail.com>
2026-05-30 07:43:50 +0530

    DataTypes-Summsry

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// # primitive
+// 7 types: string, Number, Boolean, Null, undefined, symbol,Bigint
+
+// Reference types (Non primitive)
+//  Arrays, Objects, Functions
+//  javascript is 
+
+const score = 100
+const scoreValue = 200.3
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+
+ const id= Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id == anotherId); 
+  const bigNumber = 6563162852n
+
+
+  const heros = ["shaktiman", "krissh","naagraj"]
+  let myObj ={
+    name: "hemant"
+    // age: 18
+  }
+  const myFunction = function(){
+    console.log("Hello World")
+  }
+  console.log(typeof outsideTemp )
